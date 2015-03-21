648b7291609ec480a63af18c1929ca974238d96f
Ion Scerbatiuc <ion@rover.com>
2015-03-21 11:53:05 -0700

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-kungfu',
-    version='0.1.1',
+    version='0.2',
     description=('A Flasky approach to distributed Django configuration'),
     long_description=open('README').read(),
     keywords='django settings overrides flasky',
